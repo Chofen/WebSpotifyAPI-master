d77ea6cc43e398c9ccc60feb8baa8b9000713f28
AlbinAhlbeck <albin.ahlbeck@gmail.com>
2021-01-09 00:34:01 +0100

    reworked code

--- demoApp/app.js
+++ demoApp/app.js
@@ -1,11 +1,61 @@
-const APIController = (function() {
-    
+
+function initMap() 
+{
     const clientId = 'aed1d37eab2043c0973e1fbd81f6aec2';
     const clientSecret = '02225197ebe24b5dbc46cbae56a40ef9';
 
+    const australia = { lat: -25.344, lng: 131.036 };
+    const sweden = { lat: 60, lng: 18};
+    var playlists = [];
+    var locations = [
+        ['Australia', -25, 131, 4],
+        ['Sweden', 60, 18, 5],
+        ['Cronulla Beach', -34.028249, 151.157507, 3],
+        ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+        ['Maroubra Beach', -33.950198, 151.259302, 1]
+      ];
+      const DOMElements = {
+          // ADD HTML ELEMENTS HERE
+        hfToken: '#hidden_token',
+       
+    };
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: australia,
+    });
+
+    init = async () => {
+
+        const token = await getToken();           
+        storeToken(token);
+
+        var infowindow = new google.maps.InfoWindow();
+
+        var marker, i;
+    
+        for (i = 0; i < locations.length; i++) {  
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+          });
+          var playlist = getPlaylistByCountry(token, locations[i][0]);
+        // add playlists into an array
+        //const tracks = await(getTracks(token, playlist.href + "/tracks"));
+          google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+              infowindow.setContent(`<iframe src="https://open.spotify.com/embed/playlist/${playlist.id}" 
+              width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`);
+               
+              //infowindow.setContent(locations[i][0])
+              infowindow.open(map, marker);
+            }
+          })(marker, i));
+      }
+    }
 
     // private methods
-    const _getToken = async () => {
+    const getToken = async () => {
 
         const result = await fetch('https://accounts.spotify.com/api/token', {
             method: 'POST',
@@ -20,8 +70,7 @@ const APIController = (function() {
         return data.access_token;
     }
     
-    const _getPlaylistByCountry = async (token, country) => {
-
+    const getPlaylistByCountry = async (token, country) => { 
         const result = await fetch(`https://api.spotify.com/v1/search?q="Top%2050%20${country}&type=playlist&limit=1&owner=spotifycharts`, {
             method: 'GET',
             headers: { 'Authorization' : 'Bearer ' + token}
@@ -29,11 +78,9 @@ const APIController = (function() {
 
         const data = await result.json();
         return data.playlists.items[0];
-    }
+    }    
 
-    
-
-    const _getTracks = async (token, tracksEndPoint) => {
+    const getTracks = async (token, tracksEndPoint) => {
 
         const limit = 5;
 
@@ -46,7 +93,7 @@ const APIController = (function() {
         return data.items;
     }
 
-    const _getTrack = async (token, trackEndPoint) => {
+    const getTrack = async (token, trackEndPoint) => {
             console.log("endpoint:" + trackEndPoint);
         const result = await fetch(`${trackEndPoint}`, {
             method: 'GET',
@@ -57,7 +104,7 @@ const APIController = (function() {
         return data;
     }
 
-    const _getWikipedia = async(endpoint) => {
+    const getWikipedia = async(endpoint) => {
         const result = await fetch(`${endpoint}`, {
             method: 'GET',
         });
@@ -65,226 +112,17 @@ const APIController = (function() {
         const data = await result.json();
         return data;
     }
-
-    return {
-        getToken() {
-            return _getToken();
-        },
-       
-        getPlaylistByCountry(token, country) {
-            return _getPlaylistByCountry(token, country);
-        },
-        getTracks(token, tracksEndPoint) {
-            return _getTracks(token, tracksEndPoint);
-        },
-        getTrack(token, trackEndPoint) {
-            return _getTrack(token, trackEndPoint);
-        },
+    
+    function storeToken(value) 
+    {
+        document.querySelector(DOMElements.hfToken).value = value;
     }
-})();
 
-
-// UI Module
-const UIController = (function() {
-
-    //object to hold references to html selectors
-    const DOMElements = {
-        buttonSubmit: '#btn_submit',
-        divSongDetail: '#song-detail',
-        hfToken: '#hidden_token',
-        divSonglist: '.song-list',
-        inputCountry: '#country',
-        spotifyPlayer: '#spotifyPlayer',
-        map: '#map'
-    };
-
-    //public methods
-    return {
-
-        //method to get input fields
-        inputField() {
-            return {
-                genre: document.querySelector(DOMElements.selectGenre),
-                playlist: document.querySelector(DOMElements.selectPlaylist),
-                tracks: document.querySelector(DOMElements.divSonglist),
-                submit: document.querySelector(DOMElements.buttonSubmit),
-                songDetail: document.querySelector(DOMElements.divSongDetail),
-                country: document.querySelector(DOMElements.inputCountry),
-                spotifyPlayer: document.querySelector(DOMElements.spotifyPlayer),
-                mapView: document.querySelector(DOMElements.map)
-            }
-        },
-
-        createPlaylist(playlistID) {
-            //const html = `<option value="${value}">${text}</option>`;
-            //document.querySelector(DOMElements.selectPlaylist).insertAdjacentHTML('beforeend', html);
-            const spotifyPrew = document.querySelector(DOMElements.spotifyPlayer)
-            spotifyPrew.innerHTML = ''; // reset
-            // create a inbedded spotify player and open the playlist
-            const html = `<iframe src="https://open.spotify.com/embed/playlist/${playlistID}" width="300" height="380"
-             frameborder="0"allowtransparency="true"
-              allow="encrypted-media"></iframe>`;
-            
-            spotifyPrew.insertAdjacentHTML('beforeend',html);
-        },
-
-        createTracks(tracks, length){
-            const spotifyPrew = document.querySelector(DOMElements.spotifyPlayer)
-            spotifyPrew.innerHTML = ''; // reset    
-            for (i = 0; i < 5; i++) {
-                const html = `<iframe src="https://open.spotify.com/embed/track/${tracks[i]}"
-                 width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
-                 spotifyPrew.insertAdjacentHTML('beforeend', html)
-            }
-        },
-
-        // need method to create a track list group item 
-        createTrack(trackID) {
-            const spotifyPrew = document.querySelector(DOMElements.spotifyPlayer);
-            const html = `<iframe src="https://open.spotify.com/embed/track/${trackID}"
-                 width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
-                spotifyPrew.insertAdjacentHTML('beforeend', html)
-        },
-
-        clearTracks()
-        {
-            const spotifyPrew = document.querySelector(DOMElements.spotifyPlayer);
-            spotifyPrew.innerHTML = '';
-        },
-
-        // need method to create the song detail
-        createTrackDetail(img, title, artist) {
-
-            const detailDiv = document.querySelector(DOMElements.divSongDetail);
-            // any time user clicks a new song, we need to clear out the song detail div
-            detailDiv.innerHTML = '';
-
-            const html = 
-            `
-            <div class="row col-sm-12 px-0">
-                <img src="${img}" alt="">        
-            </div>
-            <div class="row col-sm-12 px-0">
-                <label for="Genre" class="form-label col-sm-12">${title}:</label>
-            </div>
-            <div class="row col-sm-12 px-0">
-                <label for="artist" class="form-label col-sm-12">By ${artist}:</label>
-            </div> 
-            `;
-            detailDiv.insertAdjacentHTML('beforeend', html)
-
-        },
-        // creates the Google Maps
-        createMap(countryName) 
-        {
-
-        const map = document.querySelector(DOMElements.map);
-        map.innerHTML = '';
-        const html =               
-            `<iframe
-            width="600"
-            height="450"
-            frameborder="0" style="border:0"
-            src="https://www.google.com/maps/embed/v1/place?key=
-            AIzaSyDOM4UOXcIRRErjQuoaZqmiYZ7lsvDstig  
-            &q=${countryName}" allowfullscreen>
-            </iframe>`
-            map.insertAdjacentHTML('beforeend', html);
-
-        },
-
-        createWikipedia()
-        {
-
-        },
-
-        resetTrackDetail() {
-            this.inputField().songDetail.innerHTML = '';
-            this.inputField().spotifyPlayer.innerHTML = '';
-        },
-
-        resetCountry(){
-            
-        },
-
-        resetTracks() {
-            this.inputField().tracks.innerHTML = '';
-            this.resetTrackDetail();
-        },
-
-        resetPlaylist() {
-            this.inputField().spotifyPlayer.innerHTML = '';
-            this.resetTracks();
-        },
-        
-        storeToken(value) {
-            document.querySelector(DOMElements.hfToken).value = value;
-        },
-
-        getStoredToken() {
-            return {
-                token: document.querySelector(DOMElements.hfToken).value
-            }
-        },
-        getCountry() {
-            console.log(document.querySelector(DOMElements.inputCountry).value);
-            return document.querySelector(DOMElements.inputCountry).value
+    function getStoredToken() 
+    {
+        return {
+            token: document.querySelector(DOMElements.hfToken).value
         }
     }
-
-})();
-
-const APPController = (function(UICtrl, APICtrl) {
-
-    const DOMInputs = UICtrl.inputField();
-    // get genres on page load
-    const loadGenres = async () => {
-        //get the token
-        const token = await APICtrl.getToken();           
-        UICtrl.storeToken(token);
-        UICtrl.resetPlaylist();
-        UICtrl.resetCountry();
-       
-    }
-
-    DOMInputs.submit.addEventListener('click', async (e) => {
-        e.preventDefault();
-        // clear tracks
-        UICtrl.resetTracks();
-        const token = UICtrl.getStoredToken().token; 
-        const countryName = UICtrl.getCountry();
-     
-        // get the playlist based on a genre
-        const playlist = await(APICtrl.getPlaylistByCountry(token, countryName));  
-        console.log(playlist.href + "/tracks");
-       const tracks = await(APICtrl.getTracks(token, playlist.href + "/tracks"));
-
-        UICtrl.resetTracks();
-        for (i = 0; i < 5; i++) {
-            var track = tracks[i].track;
-            UICtrl.createTrack(track.id);
-        }
-
-        // Add an embedded google map with the selected country
-        UICtrl.createMap(countryName);
-        
-    });
-
-    return {
-        init() {
-            console.log('App is starting');
-            loadGenres();
-        }
-    }
-
-})(UIController, APIController);
-
-// will need to call a method to load the genres on page load
-APPController.init();
-
-
-
-
-
-
-
+    init();
+}
